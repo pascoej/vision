0742c6f9d4f438b2b72c8c407dc17f031fec6459
John Pascoe <pascoej@murri.ca>
2016-01-21 14:07:13 -0500

    fix webcam test

--- webcam-test.py
+++ webcam-test.py
@@ -15,21 +15,20 @@ cap = cv2.VideoCapture(0) #Mess with this to get externel usb cam
 
 while True:
   ret, frame = cap.read()
+  frame = cv2.resize(frame,(720,405))
   gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
   blurred = cv2.GaussianBlur(gray,(3,3),0) # remove noise
 
-  th3 = cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,101,2) # second from last var is block size
+  th3 = cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,33,2) # second from last var is block size
   
   auto = auto_canny(th3,1)#get edge
 
-  cv2.imshow('ok',auto)
   im2, contours, hierachy = cv2.findContours(auto,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE) # find continous lines
 
   sort_key = lambda  cnt: cv2.arcLength(cnt,True) # assume closed, sort by arc lengeth
   cnts = sorted(contours,key=sort_key)[-2:] # Only want largest 2 contour
   cv2.drawContours(frame,cnts,-1,(0,255,0),3)#draw on orig
   cv2.imshow('bg',frame)
-  cv2.ResizeWindow('bg',400,281)
 
   if (cv2.waitKey(1) == ord('q')):
     break
